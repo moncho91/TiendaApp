06fcb88d33dfb9277d51dc665e73aa012dbf890d
Moncho <moncho.villalobos91@gmail.com>
2014-11-03 18:19:12 +0100

    TiendaApp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TiendaApp::Application.config.secret_key_base = '07eff848e47c77a7c6729c02bda7576cd2d006291d9fb75abc5d5f3e2f1486046389626ef0cca1e683a8d728a5a85251e79f4c7d17972a5446aa6b36b550dd3e'
